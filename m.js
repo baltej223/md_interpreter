bd6a28667509389753b9e384c7fde9eabf4306e2
Baltej Singh <73211864+baltej223@users.noreply.github.com>
2024-07-14 13:52:50 +0530

    Update m.js

--- m.js
+++ m.js
@@ -1,29 +1,146 @@
-page.js("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js", function () {
-        page.css("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css", function () {
-            hljs.highlightAll();
+page = {
+    "title": function (Title) {
+        if (Title != undefined) {
+            try {
+                document.title = Title;
+                return true;
+            }
+            catch (error) {
+                console.log("Error in manupulating page title");
+                return false;
+            }
+        }
+        else {
+            return document.title;
+        }
+    },
+    "favicon": function (url = 'https://stackoverflow.com/favicon.ico') {
+        let link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.head.appendChild(link);
+        }
+        link.href = url;
+    },
+    "divide": function (...parts) {
+        let parent = document.createElement("div");
+        parent.style.display = "flex";
+        parent.style.flexDirection = "column";
+        let i = 1; 
+        try {
+            while (i < parts.length) { 
+                let child = document.createElement("div");
+                child.classList.add("child");
+                child.style.width = `${parts[i]}vw`; 
+                parent.appendChild(child);
+                i++;
+            }
+        } catch (error) {
+            console.error('An Error occurred at page.divide(), Error:', error);
+        }
+        $$$().then(function (el) {
+            el.appendChild(parent);
         });
+    },
+    "css": function (url, callback=function(){}) {
+        try {
+            let styl = document.createElement('link');
+            styl.rel = 'stylesheet';
+            document.head.appendChild(styl);
+            styl.href = url;
+            if (callback && typeof callback === 'function') {
+                callback(); 
+            }
+        } catch (e) {
+            let style = document.querySelector("style");
+            if (!style) {
+                style = document.createElement('style');
+                fetch(url)
+                    .then(response => response.text())
+                    .then(data => {
+                        style.innerHTML = data;
+                        document.head.appendChild(style);
+                        if (callback && typeof callback === 'function') {
+                            callback(); 
+                        }
+                    })
+                    .catch(error => console.error('Error fetching CSS:', error));
+            }
+        }
+    },
+    "js": function (uri, callback=function(){}) {
+        try {
+            let scr = document.createElement('script');
+            document.head.appendChild(scr);
+            scr.src = uri;
+            if (callback && typeof callback === 'function') {
+                scr.onload = callback; 
+            }
+        } catch (e) {
+            let scr = document.createElement('script');
+            fetch(uri)
+                .then(response => response.text())
+                .then(data => {
+                    scr.innerHTML = data;
+                    document.head.appendChild(scr);
+                    if (callback && typeof callback === 'function') {
+                        callback(); 
+                    }
+                })
+                .catch(error => console.error('Error fetching JS:', error));
+        }
+    }
+
+};
+Element.prototype.renderit = function (content) {
+    if (this instanceof Element) {
+
+        this.innerHTML += content;
+    } else {
+        console.error("renderin method used on wrong type.");
+    }
+};
+function $$$(selector = "body", index = 0, returnarray = false) {
+    return new Promise((resolve, reject) => {
+        const onDOMContentLoaded = () => {
+            try {
+                if (!returnarray) {
+                    var element = document.querySelectorAll(selector)[index];
+                    if (element) {
+                        resolve(element);
+                    } else {
+                        reject(`Element with selector '${selector}' and index ${index} not found.`);
+                    }
+                } else {
+                    var elements = document.querySelectorAll(selector);
+                    if (elements.length > 0) {
+                        resolve(Array.from(elements)); // Resolve with an array of elements
+                    } else {
+                        reject(`No elements found with selector '${selector}'.`);
+                    }
+                }
+            } catch (error) {
+                reject(`Error selecting element: ${error}`);
+            }
+        };
+
+        if (document.readyState === 'loading') {
+            document.addEventListener("DOMContentLoaded", onDOMContentLoaded);
+        } else {
+            onDOMContentLoaded();
+        }
     });
-try {
-    page.js("https://code.jquery.com/jquery-3.7.1.slim.min.js");
-
 }
-catch (e) {
-    "Unable to import jquery,Error" + e.c();
-}
-
-page.css("https://baltej223.github.io/md_interpreter/files/css.css",function(){
-
-});
 
 md = {
     'load': function (url) {
         getData(url);
     },
     'process':function(data){
-        pr1(data,true);
+        pr1(data);
     }
 }
-page.js("https://baltej223.github.io/personal/personal.js",function(){
 function getData(url) {
     document.addEventListener("DOMContentLoaded", function () {
         if (url) {
@@ -46,7 +163,7 @@ function getData(url) {
 }
 
 const escapeHtml = str => str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-function pr1(str,toreturn=false) {
+function pr1(str) {
 
     str = str.replace(/<script\b[^>]*>[\s\S]*?<\/script>/gi, '')
 
@@ -54,9 +171,9 @@ function pr1(str,toreturn=false) {
 
     str = str.replace(/<br\s*\/?>/gi, "\n");
 
-    pr2(escapeHtml(str),toreturn);
+    pr2(escapeHtml(str));
 }
-function pr2(data,toreturn=false) {
+function pr2(data) {
 
     data = data.replace(/^(\s*)######\s+(.+)/gm, "$1<h6>$2</h6>");
     data = data.replace(/^(\s*)#####\s+(.+)/gm, "$1<h5>$2</h5>");
@@ -98,12 +215,13 @@ function pr2(data,toreturn=false) {
     data = data.replace(/\n{2,}/g, '<br>');
 
     data = data.replace(/!{t}/g, '<svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" class="check-icon"><path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path></svg>');
-    
-  deliver(data,toreturn);
+
+    deliver(data);
 }
 
 function deliver(data,tort) {
     if (!tort){
+
     $$$().then(function (body) {
         body.update(`
             <div class='parent'> 
@@ -114,6 +232,62 @@ function deliver(data,tort) {
 }
 else{
 return data;
+}
+
+    page.js("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js", function () {
+        page.css("https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css", function () {
+            hljs.highlightAll();
+        });
+    });
+}
+function ready(callback) {
+
+    if (document.readyState !== 'loading') {
+        callback();
+    } else { 
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+}
+
+try {
+    page.js("https://code.jquery.com/jquery-3.7.1.slim.min.js");
+
+}
+catch (e) {
+    "Unable to import jquery,Error" + e.c();
+}
+
+page.css("https://baltej223.github.io/md_interpreter/files/css.css",function(){
+
+});
+
+    function unescapeHtml(html) {
+        let textarea = document.createElement("textarea");
+        textarea.innerHTML = html;
+        setTimeout(function(){
+            document.removeChild(textarea);
+        },100);
+        return textarea.value;
+    }
+function copy(data) {
+
+    let unescapedText = unescapeHtml(data);
+
+    navigator.clipboard.writeText(unescapedText)
+        .then(() => {
+            console.log("Text copied to clipboard:", unescapedText);
+
+        })
+        .catch(err => {
+            console.error("Failed to copy text to clipboard:", err);
+
+        });
+}
+String.prototype.copy = function() {
+    copy(this);
+};
+Function.prototype.ready = function(){
+    ready(this);
 }
 
 window.onload = function() {
@@ -127,5 +301,3 @@ window.onload = function() {
         });
     });
 };
-
-});//ending of callback function passed as a parameter to page.js which will only fire once personal.js (https://baltej223.github.io/personal/personal.js) will be fully imported
